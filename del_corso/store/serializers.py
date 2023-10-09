706d9bcc1143e351f405a61b830e6fd016b61f48
Andrew Strongin <andrew.strongin@innowise-group.com>
2023-10-09 11:12:59 +0300

    DC-1 Add discounts for ProductViewSet

--- del_corso/store/serializers.py
+++ del_corso/store/serializers.py
@@ -1,3 +1,5 @@
+from django.apps import apps
+from django.utils import timezone
 from rest_framework import serializers
 
 from store.models import Size, Color, TypeCategory, SeasonCategory, Product, ProductImage
@@ -39,11 +41,29 @@ class ProductSerializer(serializers.ModelSerializer):
     season_category = SeasonCategorySerializer()
     type_category = TypeCategorySerializer()
     image_paths = serializers.SerializerMethodField()
+    discount = serializers.SerializerMethodField()
 
     def get_image_paths(self, obj):
         product_images = obj.productimage_set.all()
         image_paths = [image.image.path for image in product_images]
         return image_paths
+
+    def get_discount(self, obj):
+        product_discount_model = apps.get_model('discounts', 'ProductDiscount')
+        active_discount = product_discount_model.objects.filter(
+            product=obj,
+            discount__start_date__lte=timezone.now().date(),
+            discount__end_date__gte=timezone.now().date()
+        ).first()
+
+        if active_discount:
+            return {
+                'discount_price': active_discount.discount.discount_price,
+                'start_date': active_discount.discount.start_date,
+                'end_date': active_discount.discount.end_date,
+            }
+
+        return None
 
     class Meta:
         model = Product
