d12447ac5d358b519fb8646fbf44e3ca6652a1d3
Andrew Strongin <andrew.strongin@innowise-group.com>
2023-12-22 23:38:22 +0300

    Adjust docker compose

--- del_corso/del_corso/urls.py
+++ del_corso/del_corso/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
@@ -17,3 +18,4 @@ urlpatterns = [
 ]
 
 urlpatterns += routers.urls
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
